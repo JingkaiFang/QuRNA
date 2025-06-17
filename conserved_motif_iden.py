2a0924d490741940bc174f96c9a10e7d04abef0b
JingkaiFang <105834102+JingkaiFang@users.noreply.github.com>
2025-06-17 11:26:58 +0800

    Update conserved_motif_iden.py

--- conserved_motif_iden.py
+++ conserved_motif_iden.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Jing-kai Fang
+# Copyright 2025 Jing-Kai Fang
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
